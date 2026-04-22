a67f7c6bbee9de0364a766b1a3f5bcd62a6cad30
Joaobachmann <joaobachmannalun@gmail.com>
2026-04-22 14:16:20 -0300

    Feat #6: Removi imagens com a formatação errada que estava causando conflito com o pwa. Reconfigurei ele e adicionei as imagens corretas

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+
+import { registerSW } from 'virtual:pwa-register'
 
 const app = createApp(App)
 
@@ -12,3 +13,13 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    console.log('🆕 Nova versão disponível do app')
+  },
+  onOfflineReady() {
+    console.log('📦 App pronto para uso offline')
+  }
+})
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { VitePWA } from 'vite-plugin-pwa' // 👈 adiciona isso
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
@@ -12,33 +12,50 @@ export default defineConfig({
 
     VitePWA({
       registerType: 'autoUpdate',
+
+      devOptions: {
+        enabled: false
+      },
+
       manifest: {
-        name: 'MaruJob',
-        short_name: 'MaruJob',
+        name: 'MaruJobs',
+        short_name: 'MaruJobs',
         description: 'Plataforma de freelances',
+
         theme_color: '#5b3cc4',
         background_color: '#ffffff',
+
         display: 'standalone',
+        start_url: '/',
+        scope: '/',
+
         icons: [
           {
-            src: '/icon-192.png',
+            src: '/icons/icon-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
-            src: '/icon-512.png',
+            src: '/icons/icon-512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
+      },
+
+      workbox: {
+        cleanupOutdatedCaches: true,
+
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}']
       }
     })
-
   ],
 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
