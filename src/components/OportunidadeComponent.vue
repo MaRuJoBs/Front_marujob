d63e012fffa8087118a34c0608a24381fead3094
omtramos <omtramos2026@gmail.com>
2026-04-22 09:18:02 -0300

    HTML Feat 7 feito

---
+++ src/components/OportunidadeComponent.vue
@@ -0,0 +1,91 @@
+<script setup>
+</script>
+<template>
+  <div>
+    <header>
+      <div>
+        <input type="text" placeholder="Buscar oportunidades..." />
+        <button>Filtro</button>
+      </div>
+
+      <div>
+        <button>Todas</button>
+        <button>Design</button>
+        <button>Design</button>
+        <button>Design</button>
+        <button>Design Gráfico</button>
+        <button>R$500 - R$250</button>
+        <button>Remover filtros</button>
+      </div>
+    </header>
+    <main>
+      <section>
+        <div>
+          <h3>Design Gráfico</h3>
+          <button>❤</button>
+        </div>
+
+        <p>Design Pro</p>
+
+        <div>
+          <p>Centro, SP</p>
+          <p>29/04/2026 • 9:00 - 15:00</p>
+          <p>Home office</p>
+        </div>
+
+        <div>
+          <button>Ver detalhes</button>
+          <span>R$ 200</span>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h3>Design Gráfico</h3>
+          <button>❤</button>
+        </div>
+
+        <p>Design Pro</p>
+
+        <div>
+          <p>Centro, SP</p>
+          <p>29/04/2026 • 9:00 - 15:00</p>
+          <p>Home office</p>
+        </div>
+
+        <div>
+          <button>Ver detalhes</button>
+          <span>R$ 200</span>
+        </div>
+      </section>
+      <section>
+        <p>Salve oportunidades em seus favoritos! Aperte no ❤ para adicionar</p>
+      </section>
+
+      <!-- CARD -->
+      <section>
+        <div>
+          <h3>Design Gráfico</h3>
+          <button>♡</button>
+        </div>
+
+        <p>Design Pro</p>
+
+        <div>
+          <p>Centro, SP</p>
+          <p>29/04/2026 • 9:00 - 15:00</p>
+          <p>Home office</p>
+        </div>
+
+        <div>
+          <button>Ver detalhes</button>
+          <span>R$ 200</span>
+        </div>
+      </section>
+
+    </main>
+
+  </div>
+</template>
+
+<style scoped>
+</style>
