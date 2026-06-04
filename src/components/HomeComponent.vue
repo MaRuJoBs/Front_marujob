f6574d7e0d53faa076034ed64e17972c905a3c51
RicbaronR <ric.baron01@gmail.com>
2026-06-04 15:35:09 -0300

    FEAT: erros DEV arrumados

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,55 +1,98 @@
 <script setup>
+import { ref, onMounted, computed } from 'vue'
+
+
+const jobs = ref([])
+const page = ref(1)
+const totalPages = ref(1)
+const temProximaPagina = ref(false)
+const buscarJobs = async () => {
+  const res = await fetch(`http://127.0.0.1:8000/api/freelances/?page=${page.value}`)
+  const data = await res.json()
+
+    console.log("PAGE:", page.value)
+  console.log("RESULTS:", data.results)
+
+  jobs.value = [...jobs.value, ...data.results]
+  temProximaPagina.value = data.next !== null
+}
+onMounted(() => {
+  buscarJobs()
+})
+
+const principais = computed(() => {
+  return jobs.value.filter((job) => job.preco >= 50)
+})
+
+const limite = ref(3)
+const principaisLimitados = computed(() => {
+  return principais.value.slice(0, limite.value)
+})
+
+
+
+const gruposPrincipais = computed(() => {
+  const grupos = []
+
+  for (let i = 0; i < principais.value.length; i += 3) {
+    grupos.push(principais.value.slice(i, i + 3))
+  }
+
+  return grupos
+})
+const slideAtual = ref(0)
 </script>
 
 <template>
-    <main class="home">
-  <section class="principais">
-    <h2>Principais</h2>
+  <main class="home">
+    <section class="principais">
+      <h2>Principais</h2>
 
-    <div class="card">
-  <div class="retangulo"></div>
+      <div v-for="job in principaisLimitados" :key="job.id" class="card">
+        <div class="retangulo"></div>
 
-  <div class="info">
-    <h3>Design Gráfico</h3>
-    <p>Design Pro</p>
+        <div class="info">
+          <h3>{{ job.titulo }}</h3>
+          <p>{{ job.tag }}</p>
 
-    <div class="preco">
-      <span>R$100</span>
-      <p class="horas">4 horas</p>
-    </div>
-  </div>
-
-  <span class="arrow">›</span>
-</div>
-
-   
-  </section>
-
- 
-  <div class="dots">
-    <span class="active"></span>
-    <span></span>
-    <span></span>
-    <span></span>
-  </div>
-
-
-  <section class="mais">
-    <h3>Mais oportunidade...</h3>
-
-    <div class="scroll">
-      <div class="mini-card">
-        <h4>Tradução</h4>
-        <p>Empresa 1</p>
-        <span>R$60</span>
-        <p>2 horas</p>
-        <button>Ver Detalhes</button>
+          <div class="preco">
+            <span>R${{ job.preco }}</span>
+            <p>•</p>
+            <p class="horas">{{ job.tempo }} horas</p>
+          </div>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-</main>
-</template> 
+    <div class="dots">
+      <span class="active"></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+
+    <section class="mais">
+      <h3>Mais oportunidade...</h3>
+
+      <div class="scroll">
+        <div class="mini-card" v-for="job in jobs" :key="job.id">
+          <h4>{{ job.titulo }}</h4>
+          <p>Empresa</p>
+          <!--<p>{{ job.empresa }}</p>-->
+          <span>R${{ job.preco }}</span>
+          <p>{{ job.tempo }} horas</p>
+          <button>Ver Detalhes</button>
+        </div>
+      </div>
+      <button 
+  v-if="temProximaPagina"
+  @click="page++; buscarJobs()"
+>
+  Ver mais
+</button>
+    </section>
+  </main>
+</template>
 <style scoped>
 body {
   margin: 0;
@@ -57,56 +100,56 @@ body {
   background: #ebebeb;
 }
 main {
-  background-color: #E0D1F9;
+  background-color: #e0d1f9;
   border-radius: 60px 60px 0 0;
-  margin-top: -55px;
+  margin-top: -85px;
   position: relative;
   z-index: 5;
-  padding-top: 20px;
+  height: 100vh;
 }
 .home {
   padding: 0 16px 16px 16px;
 }
 
-
+/* PRINCIPAIS */
 
 .principais h2 {
   color: rgb(226, 205, 205);
   background: #5b3cc4;
   text-align: center;
   max-width: 190px;
-  font-size: 18px;
+  font-size: 22px;
   padding: 5px 18px;
   border-radius: 0 0 10px 10px;
   margin: 0 auto;
 }
 .retangulo {
-  width: 60px;
-  height: 60px;
+  width: 80px;
+  height: 80px;
   background-color: #939292;
   border-radius: 10px;
-  margin: 0; 
+  margin: 0;
 }
 .card {
-  background: #D8D2E6;
+  background: #d8d2e6;
   border-radius: 12px;
-  padding: 25px 12px 25px 12px; 
+  padding: 25px 12px 25px 12px;
   margin-top: 12px;
   display: flex;
   align-items: center;
-  gap: 12px; 
+  gap: 12px;
 }
 
 .card h3 {
   margin: 0;
-  font-size: 16px;
-  color: #543B91;
+  font-size: 20px;
+  color: #543b91;
 }
 
 .card p {
   margin: 2px 0;
   color: #777;
-  font-size: 10px;
+  font-size: 15px;
 }
 div.preco {
   display: flex;
@@ -114,15 +157,15 @@ div.preco {
   gap: 8px;
 }
 .card span {
-  font-size: 12px;
-  color: #543B91;
+  font-size: 15px;
+  color: #543b91;
 }
 .card .horas {
-  font-size: 10px;
+  font-size: 15px;
   color: #777;
 }
 .arrow {
-  margin-left: auto; 
+  margin-left: auto;
   font-size: 50px;
   color: #5b3cc4;
 }
@@ -146,9 +189,10 @@ div.preco {
   background: #5b3cc4;
 }
 
+/* MAIS OPORTUNIDADES */
 .mais h3 {
   margin: 8px 0;
-  color: #49357B;
+  color: #49357b;
   font-size: 15px;
 }
 
@@ -156,37 +200,53 @@ div.preco {
   display: flex;
   overflow-x: auto;
   gap: 10px;
+
+  scroll-snap-type: x mandatory;
+  -webkit-overflow-scrolling: touch;
 }
 
 .mini-card {
   background: #fff;
   border-radius: 10px;
-  padding: 10px 12px 20px 10px;
-  color: #49357B;
+  padding: 20px 15px 20px 12px;
+  color: #49357b;
+
+  width: 150px;
+  height: 210px;
+  flex-shrink: 0;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
+
 .mini-card span {
   font-size: 18px;
-  color: #49357B;
+  color: #49357b;
 }
 
 .mini-card h4 {
-  margin: 0;
-  font-size: 19px;
+  font-size: 20px;
+  line-height: 1.2;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  height: 2.4em;
 }
 
 .mini-card p {
   margin: 4px 0;
-  font-size: 10px;
-  color: #766A94;
-} 
+  font-size: 15px;
+  color: #766a94;
+}
 
 .mini-card button {
   border: none;
   background: #5b3cc4;
   color: white;
-  border-radius: 10px;
-  padding: 6px 8px;
-  margin: 15px auto 0 auto; 
-  display: block; 
+  border-radius: 15px;
+  padding: 8px 16px;
+  margin: 15px auto 0 auto; /* 🔥 isso centraliza */
+  display: block; /* 🔥 importante */
 }
 </style>
