efb8112fa609c3ac857d6630685d27a6159ab3ad
omtramos <omtramos@gmail.com>
2026-08-11 15:13:59 -0300

    Vagas feitas

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
 <template>
-  <div class="app">
-
-
-    <router-view />
-
-    <Footer v-if="!$route.meta.hideFooter" />
-
-
-  </div>
+  <div class="app"><vagasComponent /> <Footer v-if="!$route.meta.hideFooter" /></div>
 </template>
 
 <script setup>
-
+import vagasComponent from './components/VagasComponent.vue'
 import Footer from './components/Footer.vue'
-
-
 </script>
 
 <style scoped>
-
 * {
   margin: 0;
   padding: 0;
@@ -34,5 +23,4 @@ body {
   max-width: 420px;
   margin: 0 auto;
 }
-
 </style>
